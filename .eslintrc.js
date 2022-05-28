13654d59b41f23037e2bb4106eba76c58ab99d60
Aleksei Sergienko <aleksei.sergienko@antek-aad.com>
2022-05-28 14:29:32 +0300

    lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
-  extends: ['airbnb'],
+  extends: ["airbnb-base", "prettier", "plugin:node/recommended"],
+  plugins: ["prettier"],
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      {
+        version: ">=13.0.0",
+        ignores: ["modules"],
+      },
+    ],
+    "import/prefer-default-export": 0,
+    "import/extensions": [
+      "error",
+      {
+        js: "ignorePackages",
+      },
+    ],
+  },
 };
